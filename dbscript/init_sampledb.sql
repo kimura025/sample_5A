f7460f8dbc0df0eb633289cfeb044171de6887fb
soma.nagakura <SOUMA.NAGAKURA@grp.tmnf.jp>
2025-08-18 13:11:38 +0900

    home.jspとsampledbを修正しました

--- dbscript/init_sampledb.sql
+++ dbscript/init_sampledb.sql
@@ -25,8 +25,8 @@ INSERT INTO sampledb.shop(shop_name) VALUES('店A');
 INSERT INTO sampledb.shop(shop_name) VALUES('店B');
 INSERT INTO sampledb.shop(shop_name) VALUES('店C');
 
-INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price) VALUES(1, 1, '店Aの品物A', '店Aの品物A説明', 100);
-INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price) VALUES(1, 2, '店Aの品物B', '店Aの品物B説明', 1000);
-INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price) VALUES(1, 3, '店Aの品物C', '店Aの品物B説明', 10000);
-INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price) VALUES(3, 1, '店Cの品物A', '店Cの品物A説明', 999999);
-INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price) VALUES(3, 2, '店Cの品物B', '店Cの品物B説明', 9999999);
+INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price,item_regist) VALUES(1, 1, '店Aの品物A', '店Aの品物A説明', 100,0);
+INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price,item_regist) VALUES(1, 2, '店Aの品物B', '店Aの品物B説明', 1000,0);
+INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price,item_regist) VALUES(1, 3, '店Aの品物C', '店Aの品物B説明', 10000,1);
+INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price,item_regist) VALUES(3, 1, '店Cの品物A', '店Cの品物A説明', 999999,1);
+INSERT INTO sampledb.item(shop_id, item_id, item_name, item_describe, item_price,item_regist) VALUES(3, 2, '店Cの品物B', '店Cの品物B説明', 9999999,1);
